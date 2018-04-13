c0870a51f25551653fc32133cd5d7cd84ba80367
Jose Cabo <jcabo@comscore.com>
2018-04-13 12:11:00 +0200

    Updates Generic Plugin binary to version 2.6.4.180413.

--- src/comscore.js
+++ src/comscore.js
@@ -1,6 +1,6 @@
 // @flow
 import {BasePlugin, MediaType, Utils} from 'playkit-js'
-import ns_ from "../bin/streamsense.plugin.min.js"
+import ns_ from "../bin/comScore-JS-GenericPlugin"
 
 /**
  * Your class description.
