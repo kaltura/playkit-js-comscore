4986fbcb679ee3e65a2fe1c05b6e9237c36f3e36
Gilad Nadav <gilad.nadav@kaltura.com>
2023-05-11 01:37:36 +0300

    fix flow

--- src/comscore.js
+++ src/comscore.js
@@ -1,5 +1,5 @@
 // @flow
-import {core, BasePlugin} from 'kaltura-player-js';
+import {core, BasePlugin} from '@playkit-js/kaltura-player-js';
 import ns_ from '../bin/streamsense.plugin.min.js';
 const {MediaType, Utils, AdBreakType, Error, FakeEvent} = core;
 
